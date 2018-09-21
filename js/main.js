abbdee138291559cda6bda2b6624b792bb1da7eb
SantanuDatta <40891517+SantanuDatta@users.noreply.github.com>
2018-09-21 20:48:35 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,12 +4,21 @@ $(window).scroll(function(){
 	$('body').scrollspy({ target: '#navbarSupportedContent',  offset: '90' });
 	
 	var scroll = new SmoothScroll('a[href*="#"]');
+	
 });
 
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 10){
+		$('.abc').addClass('scrolled');
+	} else {	
+		$('.abc').removeClass('scrolled');
+	};
+});
 
+	
 $(function () {
 	$("body").niceScroll({
-        cursorcolor:"#368afe",
+        cursorcolor:"#dea128",
         cursorwidth:"10px",
         cursorborder:"0px",
 		cursorfixedheight: 100,
